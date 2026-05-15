b3986d50c0acc99d4b5a0940b73bdc3ea011b6fc
carolin3-11 <carolinestephanie617@gmail.com>
2026-05-15 19:45:18 -0300

    Initial commit

---
+++ prj01/src/App.jsx
@@ -0,0 +1,41 @@
+import habitos from './assets/sw-book.jpg'
+import felizes from './assets/eva.jpg'
+import './App.css'
+import Card from './components/card'
+import maldicao from './assets/amaldicao.jpg'
+import erauma from './assets/eraumavez.jpg'
+import principe from './assets/oprincipecruel.jpg'
+import reipeverso from './assets/oreiperverso.jpg'
+import rainha from './assets/arainhadonada.jpg'
+import eu from './assets/eu_convertido.jpg'
+
+function App() {
+
+
+  
+return (
+<>
+ <Card book={eu} title="Caroline Stephanie Paulino de Campos"
+  titulo="OBJETIVOS"
+  description="Meu objetivo é adquirir experiência profissional, desenvolver minhas habilidades e contribuir de forma positiva para o crescimento da empresa, sempre buscando aprendizado, responsabilidade e evolução profissional."
+  descricao="ANALISE E DESENVOLVIMENTO DE SISTEMAS"
+  formacao="FORMAÇÂO ACADEMICA"
+  anosum="2021-2023- TECNICO INTEGRADO EM MECANICA"
+  mecanica="MECANICA INDRUSTRIAL"
+  anos="2025-2028 ADS IFSP"
+  texto="LINKEDIN" 
+  email="carolinestephanie617@gmail.com"
+  autor="FUTURE DEV"
+  github="GITHUB"
+  experiencia="EXPERIENCIA"
+  aprendiz="Jovem Aprendiz Lojas Torra"
+  jp="Iniciei a jornada de Jovem Aprendiz nas Lojas TORRA em março de 2026.Ser jovem aprendiz é uma oportunidade de aprender e crescer profissionalmente enquanto ainda se está estudando.
+"
+   />
+
+    </> 
+  )
+}
+
+export default App
+
